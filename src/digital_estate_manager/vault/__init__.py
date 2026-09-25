99026fe3439e669e83d8b50b471c7efe077a50e0
JingJing Li <lijingjing416@gmail.com>
2026-09-25 10:58:42 +0200

    pitch update + functional my wish drop down list

--- src/digital_estate_manager/vault/__init__.py
+++ src/digital_estate_manager/vault/__init__.py
@@ -1,3 +1,19 @@
-from digital_estate_manager.vault.storage import calculate_metrics, get_default_assets, monthly_cost_chf
+from digital_estate_manager.vault.storage import (
+    WISH_OPTIONS,
+    calculate_metrics,
+    get_default_assets,
+    join_wish,
+    monthly_cost_chf,
+    save_wishes,
+    split_wish,
+)
 
-__all__ = ["calculate_metrics", "get_default_assets", "monthly_cost_chf"]
+__all__ = [
+    "WISH_OPTIONS",
+    "calculate_metrics",
+    "get_default_assets",
+    "join_wish",
+    "monthly_cost_chf",
+    "save_wishes",
+    "split_wish",
+]
